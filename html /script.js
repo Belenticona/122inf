0f126113e23b326dc89be017e15e5c5e5aa482ce
Belenticona <119267679+Belenticona@users.noreply.github.com>
2024-01-08 14:50:21 +0000

    mini proyecto

---
+++ html /script.js
@@ -0,0 +1,7 @@
+function  agragartarea ()
+{ var tarea= document.getElementById("nueva tarea").value, aprender;
+var nuevoItem= document.createElement("li");
+nuevoItem.textContent=tarea;
+document.getElementById("tarea-lista").appendChild(nuevoItem);
+document.getElementByI ("").value
+}
