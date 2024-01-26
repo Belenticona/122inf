87bdfdb16acdb56e2bd6929f28411ad612e35f41
Belenticona <119267679+Belenticona@users.noreply.github.com>
2024-01-26 08:46:56 -0400

    Delete semna2  directory

--- semna2 /miniProyecto/script.js
+++
@@ -1,7 +0,0 @@
-function  agragartarea ()
-{ var tarea= document.getElementById("nueva tarea").value, aprender;
-var nuevoItem= document.createElement("li");
-nuevoItem.textContent=tarea;
-document.getElementById("tarea-lista").appendChild(nuevoItem);
-document.getElementByI ("").value
-}
